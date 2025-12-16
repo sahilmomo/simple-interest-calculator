e43e08d639f2bceb36b6bb03288191c6a665f702
Sahil Pal <sahilpalsitu@gmail.com>
2025-12-16 13:20:29 +0530

    Refactor user prompts and enhance script comments
    Updated prompts for user input and improved comments.

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-echo "Enter Principal:"
-read p
-echo "Enter Rate of Interest:"
-read r
-echo "Enter Time:"
-read t
+# This script calculates simple interest given principal,
+# annual rate of interest and time period in years.
+# Do not use this in production. Sample purpose only.
+# Author: Upkar Lidder (IBM)
+# Additional Authors:
+# sahilmomo
 
-si=$((p*r*t/100))
-echo "Simple Interest is: $si"
+echo "Enter the principal:"
+read p
+echo "Enter rate of interest per year:"
+read r
+echo "Enter time period in years:"
+read t
+s=`expr $p \* $t \* $r / 100`
+echo "The simple interest is:"
+echo $s
